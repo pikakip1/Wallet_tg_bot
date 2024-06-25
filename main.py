032aca0b658a10f2a16c70564696d99ece5cbe4c
vladislav <pikakip222@mail.ru>
2024-06-25 17:07:19 +0300

    add router statistic

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from config import config
-from database import async_db_manager
 from handlers.common import common_router
 from handlers.menu import menu
+from handlers.statistic import statistic
 
 
 async def main():
@@ -15,7 +15,7 @@ async def main():
 
     bot = Bot(token=config.TG_API_KEY.get_secret_value())
     dp = Dispatcher(storage=MemoryStorage())
-    dp.include_routers(common_router, menu)
+    dp.include_routers(common_router, menu, statistic)
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
