c01aba0a22480fd8a7bc2764e347148ea18f70a8
vladislav <pikakip222@mail.ru>
2024-06-25 17:05:12 +0300

    moved database queries to DAO

--- services/statistic.py
+++ services/statistic.py
@@ -1,77 +1,55 @@
-from datetime import date
-
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from DAL.wallet_repository import WalletRepository
 from database import async_db_manager
 
 
-class WalletQueries:
+class WalletService:
     def __init__(self, account_id: int):
         self.account_id = account_id
+        self.wallet_repository = WalletRepository(account_id)
 
-    async def get_statistic_category(
+    async def get_statistic_categories(
             self,
             session: AsyncSession,
             wallet_name: str,
-            category_name: str,
-            month: int,
-            year: int
+
     ):
-
-        if month + 1 > 12:
-            year_end = year + 1
-            month_end = 1
-        else:
-            year_end = year
-            month_end = month + 1
-
-        date_start = date(year=year, month=month, day=1)
-        date_end = date(year=year_end, month=month_end, day=1)
-
-        statistic_category_result = await session.execute(
-            text(
-                'SELECT amount, date_transaction, comment '
-                'FROM transactions '
-                'JOIN categories ON transactions.category_id = categories.id '
-                'JOIN wallets ON transactions.wallet_id = wallets.id '
-                'WHERE wallet_name = :wallet_name AND wallets.account_id = :account_id '
-                'AND category_name = :category_name '
-                'AND date_transaction >= :date_start '
-                'AND date_transaction < :date_end '
-                'ORDER BY date_transaction DESC'
-            ), {
-                'wallet_name': wallet_name,
-                'category_name': category_name,
-                'date_start': date_start,
-                'date_end': date_end,
-                'account_id': self.account_id,
-            }
-        )
-        return statistic_category_result.fetchall()
-
-    async def get_last_records(self, wallet_name: str, count_records: int = 10):
-        async with async_db_manager.async_session_factory() as session:
-            async with session.begin():
-                last_records = await session.execute(
-                    text(
-                        'SELECT amount, date_transaction, comment, categories.category_name'
-                        'FROM transactions'
-                        'JOIN categories ON transactions.category_id = categories.id'
-                        'JOIN wallets ON transactions.wallet_id = wallets.id'
-                        'WHERE wallets.wallet_name = :wallet_name AND wallets.account_id = :account_id'
-                        'LIMIT :count_records'
-                        'ORDER BY date_transaction DESC'
-                    ), {'wallet_name': wallet_name, 'count_records': count_records, 'account_id': self.account_id}
-                )
-                return last_records.fetchall()
-
-    async def get_amount(self, session: AsyncSession, name_wallet: str):
-        amount_result = await session.execute(
-            text(
-                'SELECT amount FROM wallets'
-                'WHERE name_wallet = :name_wallet and account_id = :account_id'
-            ), {'name_wallet': name_wallet, 'account_id': self.account_id}
+        statistic_categories = await self.wallet_repository.get_category_statistics(
+            session=session,
+            wallet_name=wallet_name
         )
 
-        return amount_result.scalar()
+        formatted_records = {}
+        for record in statistic_categories:
+            amount, type_transaction, category = record
+            formatted_records.setdefault(category, 0)
+
+            if type_transaction == 'income':
+                formatted_records[category] += amount
+            else:
+                formatted_records[category] -= amount
+
+        formatted_output = '\n'.join([f'{category}: {amount}' for category, amount in formatted_records.items()])
+
+        return formatted_output
+
+    async def get_last_records(self, session: AsyncSession, wallet_name: str, count_records: int = 10) -> list[str]:
+        records = await self.wallet_repository.get_last_records(
+            session=session,
+            wallet_name=wallet_name,
+            count_records=count_records
+        )
+
+        formatted_records = []
+        for record in records:
+            formatted_records.append(f'Дата {record[1]}')
+            formatted_records.append(f'Сумма {record[0]}')
+            formatted_records.append(f'Категория {record[-1]}')
+            formatted_records.append(f'Комментарий {record[2]}')
+            formatted_records.append('-----')
+        return formatted_records
+
+    async def get_wallet_balance(self, session: AsyncSession, wallet_name: str):
+        return await self.wallet_repository.get_wallet_amount(session, wallet_name)
