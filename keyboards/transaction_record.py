0bcac605dbe4937c4057f8ab5d5e1fa3a3ae2640
vladislav <pikakip222@mail.ru>
2024-06-25 17:02:22 +0300

    add make_menu_keyboard method

--- keyboards/transaction_record.py
+++ keyboards/transaction_record.py
@@ -7,13 +7,28 @@ def make_cancel_btn():
 
 
 def make_start_button():
-    return ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='menu')]], resize_keyboard=True)
+    return ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='/menu')]], resize_keyboard=True)
 
 
-def make_menu_keyboard(chose_menu: list[str]) -> ReplyKeyboardMarkup:
-    row = [KeyboardButton(text=category) for category in chose_menu]
+def make_transaction_btn():
+    types_transaction = ['Доход', 'Расход']
+
+    row = [KeyboardButton(text=type_transaction) for type_transaction in types_transaction]
     row.append(KeyboardButton(text='Отмена'))
     return ReplyKeyboardMarkup(keyboard=[row], resize_keyboard=True)
+
+
+def make_menu_keyboard() -> ReplyKeyboardMarkup:
+    chose_menu = ['Добавить запись', 'Баланс', 'Статистика', 'Добавить кошелек']
+    cancel_button = 'Отмена'
+
+    keyboard = [
+        [KeyboardButton(text=chose_menu[0]), KeyboardButton(text=chose_menu[1]), KeyboardButton(text=chose_menu[2])],
+        [KeyboardButton(text=chose_menu[3])],
+        [KeyboardButton(text=cancel_button)]
+    ]
+
+    return ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True)
 
 
 def get_category_inline_keyboard(categories: list[str]) -> InlineKeyboardMarkup:
@@ -30,7 +45,7 @@ def get_category_inline_keyboard(categories: list[str]) -> InlineKeyboardMarkup:
 
 def get_menu_statistic_inline_keyboard() -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
-    categories_statistics = ['Траты по категориям за месяц', 'Счет', 'Последние записи']
+    categories_statistics = ['По категориям', 'Последние записи']
     for category_statistic in categories_statistics:
         builder.add(InlineKeyboardButton(
             text=category_statistic,
